ad5a8973a43c02c02299ed37b5ac4febb8e8558e
mhallaq <malhallaq@gmail.com>
2020-11-28 01:55:28 +0000

    implement exercise003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,28 +1,46 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+  return nums.map((item) => Math.pow(item, 2));
 }
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
+  const resultString = words.reduce((acc, curr, index) => {
+    return index === 0
+      ? acc + curr
+      : acc + (curr.charAt(0).toUpperCase() + curr.slice(1));
+  }, "");
+  return resultString;
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
+  const totalSubjects = people.reduce((acc, curr) => {
+    return acc + curr.subjects.length;
+  }, 0);
+  return totalSubjects;
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+  menuName = menu.filter(
+    (item) => item.ingredients.includes(ingredient) === true
+  );
+  return menuName.length > 0;
 }
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+  let result = [];
+  arr1.forEach((number) => {
+    arr2.forEach((number2) => {
+      if ((number2 === number) & !result.includes(number)) result.push(number);
+    });
+  });
+  return result.sort();
 }
 
 module.exports = {
@@ -30,5 +48,5 @@ module.exports = {
   camelCaseWords,
   getTotalSubjects,
   checkIngredients,
-  duplicateNumbers
+  duplicateNumbers,
 };
