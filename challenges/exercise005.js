f13c91557db64fdbbe6033f00e776e5a793ba6f6
mhallaq <malhallaq@gmail.com>
2020-11-29 09:02:50 +0000

    implement exercise005

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,38 +1,66 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  nPosition = nums.indexOf(n);
+  return nPosition != -1 && nPosition + 1 < nums.length
+    ? nums[nPosition + 1]
+    : null;
 };
 
-const count1sand0s = str => {
+const count1sand0s = (str) => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  strArray = str.split("");
+  onesZeros = { 0: 0, 1: 0 };
+  for (let digit of strArray) {
+    console.log(onesZeros[digit]);
+    onesZeros[digit] = (onesZeros[digit] || 0) + 1;
+  }
+  return onesZeros;
 };
-
-const reverseNumber = n => {
+const reverseNumber = (n) => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  const stringN = n.toString(10);
+  let reversedString = "";
+  for (let x = stringN.length - 1; x >= 0; x--) {
+    reversedString += stringN[x];
+  }
+  return parseInt(reversedString);
 };
 
-const sumArrays = arrs => {
+const sumArrays = (arrs) => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  const array = JSON.parse("[" + arrs + "]");
+  return array.reduce((acc, curr) => acc + curr, 0);
 };
 
-const arrShift = arr => {
+const arrShift = (arr) => {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  if (arr.length < 2) return arr;
+  let shiftedArr = arr;
+  let firstItem = arr[0];
+  let lastItem = arr[arr.length - 1];
+  shiftedArr[0] = lastItem;
+  shiftedArr[shiftedArr.length - 1] = firstItem;
+  return shiftedArr;
 };
 
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+  const valuesArr = String(Object.values(haystack));
+  return valuesArr.toLowerCase().includes(searchTerm.toLowerCase());
 };
 
-const getWordFrequencies = str => {
+const getWordFrequencies = (str) => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  const alphabitOnlyString = str.replace(/[^a-zA-Z ]/g, "");
+  const lowerCaseStr = alphabitOnlyString.toLowerCase();
+  const strArray = lowerCaseStr.split(" ");
+  let wordsObj = {};
+  for (word of strArray) {
+    wordsObj[word] = (wordsObj[word] || 0) + 1;
+  }
+  return wordsObj;
 };
 
 module.exports = {
@@ -42,5 +70,5 @@ module.exports = {
   sumArrays,
   arrShift,
   findNeedle,
-  getWordFrequencies
+  getWordFrequencies,
 };
